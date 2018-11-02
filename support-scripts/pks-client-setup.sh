31d65384709cba283312d629bc638cd5495fafad
agilderdale <agilderdale@vmware.com>
2018-11-02 16:26:50 +0000

    testing

--- support-scripts/pks-client-setup.sh
+++ support-scripts/pks-client-setup.sh
@@ -123,9 +123,6 @@ f_input_vars() {
     read -p "${parameter:-${!1}}: " $1
     echo parameter=$parameter
 #    read -p "$1 [ i.e. ${!1} ]: " $1
-     if [ $1 == '' ]; then
-        $1=
-
     echo $1 " = " ${!1}
     echo "---------------------------"
 }
