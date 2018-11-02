0b7a1411a058e0fb428184d5b5d2366d43971b15
agilderdale <agilderdale@vmware.com>
2018-11-02 22:47:01 +0000

    testing

--- support-scripts/setup-pipelines.sh
+++ support-scripts/setup-pipelines.sh
@@ -140,9 +140,15 @@ f_install_packages() {
     f_info "Updating OS and installing packages"
     add-apt-repository universe
     apt-get update ; sudo apt-get upgrade
-    apt-get install -y docker openssh-server git apt-transport-https ca-certificates curl software-properties-common build-essential
+    apt-get install -y openssh-server git apt-transport-https ca-certificates curl software-properties-common build-essential
     apt-get install -y net-tools zlibc zlib1g-dev ruby ruby-dev openssl libxslt1-dev libxml2-dev libssl-dev libreadline-dev libyaml-dev libsqlite3-dev
     apt-get install -y sqlite3 sshpass jq dnsmasq iperf3 sshpass ipcalc curl npm
+
+    curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+    add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu bionic stable"
+    apt update
+    apt-cache policy docker-ce
+    sudo apt install docker-ce
 
     f_info "Installing vmw-cli tool"
     # vwm-cli - requires nodejs >=8
