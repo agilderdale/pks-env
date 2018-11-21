be165239ab9703e97e0c51df3b4ad9747643fb9a
agilderdale <agilderdale@vmware.com>
2018-11-21 19:51:13 +0400

    testing

--- support-scripts/setup-pks-pipelines.sh
+++ support-scripts/setup-pks-pipelines.sh
@@ -147,7 +147,7 @@ f_input_vars() {
 
     var=$1
     temp=${!1}
-    if [ ! -z $COMMENT ] ; then
+    if [ ! -z "$COMMENT" ] ; then
         echo "$COMMENT"
     fi
     read -p "Set $1 [ default: ${!1} ]: " $1
